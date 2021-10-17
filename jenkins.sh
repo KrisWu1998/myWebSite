8226e874fdb625ac36f45bc3afc3cc5395be97c0
KrisWu1998 <727980015@qq.com>
2021-10-17 18:07:54 +0800

    fix: 完善脚本文件

--- jenkins.sh
+++ jenkins.sh
@@ -7,3 +7,9 @@ npm i
 
 echo "开始打包"
 npm run build
+
+echo "清空nginx下的文件"
+rm -rf '/usr/share/nginx/html/dist'
+
+echo "将jenkins打包的文件拷贝到nginx下"
+cp /var/lib/jenkins/workspace/mywebsite/dist /usr/share/nginx/html/dist
