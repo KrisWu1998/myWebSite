a8b3f66cc09693bc9b244f734e2649349375623a
KrisWu1998 <727980015@qq.com>
2021-10-26 21:59:25 +0800

    fix: 修改脚本文件

--- jenkins.sh
+++ jenkins.sh
@@ -3,13 +3,13 @@
 echo "开始执行了vue脚本文件"
 
 echo "开始下载node_module"
-sudo npm i
+npm i
 
 echo "开始打包"
-sudo npm run build
+npm run build
 
 echo "清空nginx下的文件"
-sudo rm -rf '/usr/share/nginx/html/dist'
+rm -rf '/usr/share/nginx/html/dist'
 
 echo "将jenkins打包的文件拷贝到nginx下"
-sudo cp -r '/var/lib/jenkins/workspace/mywebsite/dist' '/usr/share/nginx/html/dist'
+cp -r '/var/lib/jenkins/workspace/mywebsite/dist' '/usr/share/nginx/html/dist'
