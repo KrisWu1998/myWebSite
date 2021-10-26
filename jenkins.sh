74c9dd9da8cca55c60a6d72b4525f2bd0118eea3
KrisWu1998 <727980015@qq.com>
2021-10-26 21:43:42 +0800

    fix: 修改脚本文件

--- jenkins.sh
+++ jenkins.sh
@@ -9,7 +9,7 @@ echo "开始打包"
 npm run build
 
 echo "清空nginx下的文件"
-rm -rf '/usr/share/nginx/html/dist'
+sudo rm -rf '/usr/share/nginx/html/dist'
 
 echo "将jenkins打包的文件拷贝到nginx下"
-cp -r '/var/lib/jenkins/workspace/mywebsite/dist' '/usr/share/nginx/html/dist'
+sudo cp -r '/var/lib/jenkins/workspace/mywebsite/dist' '/usr/share/nginx/html/dist'
