a0e752aa2c5b996eff3f028e5a6dd7f06475423f
kriswu <727980015@qq.com>
2021-11-10 14:21:30 +0800

    fix: 修改脚本文件

--- jenkins.sh
+++ jenkins.sh
@@ -13,12 +13,3 @@ rm -rf '/usr/share/nginx/html/dist'
 
 echo "将jenkins打包的文件拷贝到nginx下"
 cp -r '/var/lib/jenkins/workspace/mywebsite/dist' '/usr/share/nginx/html/dist'
-
-echo "到node文件夹下载包依赖"
-cd '/usr/share/nginx/html/web_node'
-
-echo "下载node包"
-npm install
-
-echo "执行server文件"
-node server.js
